7bd05390c2a24352249b9c3e4ba0c6da9e677741
MortySan <rom4ik90@bigmir.net>
2016-11-01 16:32:42 +0200

    commit

--- src/lesson3_1hv2/Main3_1hv2.java
+++ src/lesson3_1hv2/Main3_1hv2.java
@@ -8,15 +8,22 @@ public class Main3_1hv2 {
         Scanner sc = new Scanner(System.in);
 
 
-        int w = sc.nextInt();
+//        int w = sc.nextInt();
+        int w = 4;
         int h = w*2 - 1;
+        int halfN = h / 2;
+        for (int i = 0; i < w*h; ) {
 
-        for (int i = 0; i < w*h; i++) {
-
-            int j = i%w;
-
-            if ( j < ( w/2 - Math.abs(i - w/2) +1) ) {
+            int j = i % w ;
+            int ii = i/w;
+            if ( j == 0 ) {
                 System.out.print("*");
+            }
+
+            int i1 = halfN - Math.abs(ii - halfN) + 1;
+            if ( j < i1 -1) {
+                System.out.print("*");
+                i++;
             } else {
                 System.out.println();
                 i += w - j;
